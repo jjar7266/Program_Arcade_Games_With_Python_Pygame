22ee0382495debc208ddbb06d8c93eda93c94cae
jjar7266 <jjar7266@gmail.com>
2026-08-24 17:17:35 -0400

    Add Chapter 14 sprite sheet project: constants.py
    Added the global constants module for the sprite sheet platformer project, including screen dimensions and shared color definitions. This file establishes the base values used across all other modules in the multi‑file project. Header and formatting match the established Chapter 14 style.

---
+++ chapter_14/sprite_sheets_project/constants.py
@@ -0,0 +1,39 @@
+"""
+Program Arcade Games With Python and Pygame
+Fourth Edition
+Copyright 2016
+Author: Dr. Paul Vincent Craven
+
+coded (2026) along by: Jose 'Joe' Ruiz
+
+Chapter 14: Introduction to Sprites
+
+constants.py
+"""
+# Sample Python/Pygame Programs
+# http://programarcadegames.com/
+
+# Part of a series:
+#    - move_with_walls_example.py
+#    - maze_runner.py
+#    - platform_jumper.py
+#    - platform_scroller.py
+#    - platform_moving.py
+#    - sprite_sheets/
+
+# Global constants
+
+# COLORS (RGB)
+WHITE  = (255, 255, 255)
+BLACK  = (  0,   0,   0)
+BLUE   = (  0,   0, 255)
+RED    = (255,   0,   0)
+YELLOW = (255, 255,   0)
+GREEN  = (  0, 255,   0)
+PURPLE = (128,   0, 128)
+
+# Screen Dimensions
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+
+
