c4f59702e29c2722cafdcfeec2e9c4807a29404f
jjar7266 <jjar7266@gmail.com>
2026-08-24 17:38:23 -0400

    Add Chapter 14 sprite sheet project: spritesheet_functions.py
    Added the SpriteSheet utility module used to extract individual images from larger sprite sheets. Includes the get_image() helper for slicing sprites by x/y coordinates and dimensions, with proper alpha conversion and colorkey handling. Header and formatting match the established Chapter 14 project style.

---
+++ chapter_14/sprite_sheets_project/spritesheet_functions.py
@@ -0,0 +1,67 @@
+"""
+Program Arcade Games With Python and Pygame
+Fourth Edition
+Copyright 2016
+Author: Dr. Paul Vincent Craven
+
+coded (2026) along by: Jose 'Joe' Ruiz
+
+Chapter 14: Introduction to Sprites
+
+This module is used to pull individual sprites from sprite sheets.
+
+spritesheet_functions.py
+"""
+# Sample Python/Pygame Programs
+# http://programarcadegames.com/
+
+# Part of a series:
+#    - move_with_walls_example.py
+#    - maze_runner.py
+#    - platform_jumper.py
+#    - platform_scroller.py
+#    - platform_moving.py
+#    - sprite_sheets/
+
+"""
+This class pulls smaller images out of the large sprite sheet.
+Create an instance of the class and pass in the file name as a
+parameter to the constructor. Then call getImage with the x, y location
+of the upper left corner of your sprite along with its height and width.
+You can use a drawing program to get the location of the sprite images
+you are intersted in.
+"""
+
+# Import modules
+import pygame
+import constants
+
+class SpriteSheet(object):
+    """ Class used to grab images out of a sprite sheet. """
+
+    def __init__(self, file_name):
+        """ Constructor. Pass in the file name of the sprite sheet. """
+
+        # Load the sprite sheet.
+        self.sprite_sheet = pygame.image.load(file_name).convert_alpha()
+
+    def get_image(self, x, y, width, height):
+        """
+        Grab a single image out of a larger spritesheet
+        Pass in the x, y location of the sprite
+        and the width and height of the sprite.
+        """
+
+        # Create a new blank image
+        image = pygame.Surface([width, height]).convert_alpha()
+
+        # Copy the sprite from the large sheet onto the smaller image
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+
+        # Assuming black works as the transparent color
+        image.set_colorkey(constants.BLACK)
+
+        # Return the image
+        return image
+
+
